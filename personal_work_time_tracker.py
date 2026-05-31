7660ef88d690fa57f0607054cedaa69fa52995e5
Emiranda25 <miranda25e@gmail.com>
2026-05-30 23:22:36 -0700

    added stop watch feature needs to be refined as its very jank rn

--- personal_work_time_tracker.py
+++ personal_work_time_tracker.py
@@ -1,3 +1,5 @@
+import time
+import sys
 def time_tracker(time_input, previous_time):
     hours_input, minutes_input, seconds_input = map(int, time_input.split(':'))
     hours_previous, minutes_previous, seconds_previous = map(int, previous_time.split(':'))
@@ -8,8 +10,40 @@ def time_tracker(time_input, previous_time):
     final_time = f"{final_hours:02d}:{final_minutes:02d}:{final_seconds:02d}"
     return final_time
 
-current_log = "01:00:00"
-new_entry = "02:43:25"
+def stop_watch():
+    time_start = time.time()
+    try:
+        while True:
+            recorded_time = time.time() - time_start
+            hours = int(recorded_time // 3600)
+            minutes = int((recorded_time % 3600) // 60)
+            seconds = int(recorded_time % 60)
 
-total = time_tracker(new_entry,current_log)
-print({total})
+            time_string = f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+            print(f"recorded time: {time_string}")
+
+            sys.stdout.flush() #to update the terminal and empty the cpu to prevent overflow
+            time.sleep(0.1)
+    
+    except KeyboardInterrupt:
+        print("\n\nStopwatch stopped!")
+        final_time = time.time() - time_start
+        
+        # Final time calculation
+        hours = int(final_time // 3600)
+        minutes = int((final_time % 3600) // 60)
+        seconds = int(final_time % 60)
+        
+        print(f"Final Time: {hours:02d}:{minutes:02d}:{seconds:02d}")
+        print("====================================")
+        
+
+
+# current_log = "01:00:00"
+# new_entry = "02:43:25"
+
+# total = time_tracker(new_entry,current_log)
+# print({total})
+
+if __name__ == "__main__":
+    stop_watch()
